c7e1213eeb88332d2c630cfa9285c87f49af3763
BmmNdr <abiemmibi@gmail.com>
2022-10-19 19:00:03 +0200

    Made prototype of onClick function

---
+++ app.js
@@ -0,0 +1,10 @@
+const numberButtons = document.getElementsByClassName('number');
+const display = document.querySelector("#current")
+
+for (let index = 0; index < numberButtons.length; index++) {
+    const number = numberButtons[index];
+
+    number.addEventListener('click', () => {
+        display.textContent = number.textContent;
+    })
+}
